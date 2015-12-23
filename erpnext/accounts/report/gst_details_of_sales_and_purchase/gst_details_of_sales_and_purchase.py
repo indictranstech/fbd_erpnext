53a1c1e9182cce5d12aee64313d60185b277d54d
Priya <piya@example.com>
2015-12-23 17:42:31 +0530

    [change code for multiple taxes]

--- erpnext/accounts/report/gst_details_of_sales_and_purchase/gst_details_of_sales_and_purchase.py
+++ erpnext/accounts/report/gst_details_of_sales_and_purchase/gst_details_of_sales_and_purchase.py
@@ -20,9 +20,9 @@ def validate_filters(filters):
 		frappe.throw(_("To Date must be greater than From Date"))
 
 def get_columns():
-	return [ _("ID") + ":Data:200", _("Supplier/Customer Name") + ":Data:160", _("Date") + ":Date:120",  
+	return [ _("ID") + ":Data:150", _("Supplier/Customer Name") + ":Data:180", _("Date") + ":Date:120",  
 		_("Rate") + ":Float:120", _("Sales Value") + ":Float:120", _("Purchase Value") + ":Float:120", 
-		_("GST Collected") + ":Float:150",_("GST Paid") + ":Float:150"]
+		_("GST Collected") + ":Float:120",_("GST Paid") + ":Float:120"]
 
 def get_result(filters):
 	data = []
@@ -33,8 +33,8 @@ def get_result(filters):
 		head = '"' + d['name'] + '"'
 		row = frappe.db.sql("""select COALESCE(form_id,CONCAT('<b>','Total','</b>')),(case when form_id is null then null else 
 			cs_name end) as cs_name,(case when form_id is null then null else date end) as date, ifnull((case when form_id is 
-			null then '' else rate end),0) as rate, sum(ifnull(output_sales_value,0)), sum(ifnull(input_purchase_value,0)), 
-			sum(ifnull(output_gst_collected,0)), sum(ifnull(input_gst_paid,0)) from `tabGST Details` where cs_gst_type = %s %s 
+			null then '' else rate end),0) as rate, ifnull(sum(output_sales_value),0), ifnull(sum(input_purchase_value),0), 
+			ifnull(sum(output_gst_collected),0), ifnull(sum(input_gst_paid),0) from `tabGST Details` where cs_gst_type = %s %s 
 			group by form_id with rollup""" %(head,conditions), as_list=1)
 		if row:
 			data.append(['<b>'+d['gst_type_abbreviation']+'</b>','<b>'+d['name']+'</b>','','','','','',''])
@@ -46,9 +46,10 @@ def get_result(filters):
 	if filters.get("from_date"): cond += "where date between '%s'" %filters["from_date"]
 	if filters.get("to_date"): cond += " and '%s'" %filters["to_date"]
 
-	total_value = frappe.db.sql("""select sum(ifnull(output_gst_collected,0)) ,sum(ifnull(input_gst_paid,0)) from 
+	total_value = frappe.db.sql("""select ifnull(sum(output_gst_collected),0) ,ifnull(sum(input_gst_paid),0) from 
 		`tabGST Details` %s """ %(cond),as_list=1)
-	data.append(['<b>'+'Grand Total'+'</b>','','','','','',total_value[0][0],total_value[0][1]])
+	if data:
+		data.append(['<b>'+'Grand Total'+'</b>','','','','','',total_value[0][0],total_value[0][1]])
 
 	return data 
 
--- erpnext/accounts/report/gst_report/gst_report.py
+++ erpnext/accounts/report/gst_report/gst_report.py
@@ -28,27 +28,29 @@ def get_result(filters):
 	gst_types = frappe.get_all("GST Types",fields=["name"],order_by="name")
 	
 	for d in gst_types:
-		total_out = frappe.db.sql("""select cs_gst_type,sum(ifnull(output_sales_value,0)) from `tabGST Details` where gst_type ='-GST Output' and cs_gst_type='%s' %s """%(d['name'],conditions),as_list=1)
+		total_out = frappe.db.sql("""select cs_gst_type, ifnull(sum(output_sales_value),0) from `tabGST Details` where gst_type ='-GST Output' and cs_gst_type='%s' %s """%(d['name'],conditions),as_list=1)
 		for row in total_out:
 			if row[0]!=None:
 				data.append([row[0],row[1]])
 
 
-	total_out_value = frappe.db.sql("""select sum(ifnull(output_sales_value,0)) from `tabGST Details` where gst_type = '-GST Output' %s """%(conditions),as_list=1)
+	total_out_value = frappe.db.sql("""select ifnull(sum(output_sales_value),0),ifnull(sum(output_gst_collected),0) from `tabGST Details` where gst_type = '-GST Output' %s """%(conditions),as_list=1)
 	if total_out_value:
 		data.append(['Total value of Sales Output',total_out_value[0][0]])
 
 
-	taxable_purchase = frappe.db.sql("""select sum(ifnull(input_purchase_value,0)) from `tabGST Details` where gst_type = '-GST Input' and input_rate != 0 %s """%(conditions),as_list=1)
+	taxable_purchase = frappe.db.sql("""select ifnull(sum(input_purchase_value),0),ifnull(sum(input_gst_paid),0) from `tabGST Details` where gst_type = '-GST Input' %s """%(conditions),as_list=1)
 	if taxable_purchase:
-		data.append(['Total value of taxable Purchases',taxable_purchase[0][0]])
+		data.append(['Total value of Taxable Purchases',taxable_purchase[0][0]])
 
+	if total_out_value:
+		data.append(['Output Tax Due',total_out_value[0][1]])
 
-	total = frappe.db.sql("""select sum(ifnull(output_gst_collected, 0)), sum(ifnull(input_gst_paid, 0)) from `tabGST Details` where rate != 0 %s """ %(conditions),as_list=1)
-	if total:
-		data.append(['Output Tax Due',total[0][0]])
-		data.append(['Less: Input tax and refunds claimed',total[0][1]])
-		data.append(['<b>'+'Equals: Net GST to be paid to/(refunded by) IRAS'+'</b>',total[0][0]-total[0][1]])
+	if taxable_purchase:
+		data.append(['Less: Input tax and refunds claimed',taxable_purchase[0][1]])
+
+	if total_out_value and taxable_purchase:
+		data.append(['<b>'+'Equals: Net GST to be paid to/(refunded by) IRAS'+'</b>',total_out_value[0][1] - taxable_purchase[0][1]])
 
 	data.append([])
 
